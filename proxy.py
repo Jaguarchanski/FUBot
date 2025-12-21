d53422f7c18010eed71f103bcde8dde387395c7d
Jaguarchanski <sarakhmanjakob@gmail.com>
2025-12-21 18:01:37 +0200

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,13 +1,24 @@
 import os
 
-def get_proxies():
-    http = os.getenv("PROXY_HTTP")
-    https = os.getenv("PROXY_HTTPS")
+# Очікує наступні змінні в Render Environment
+# PROXY_HOST, PROXY_PORT, PROXY_USER, PROXY_PASS, PROXY_COUNTRY
+# Наприклад:
+# PROXY_HOST=123.45.67.89
+# PROXY_PORT=8080
+# PROXY_USER=myuser
+# PROXY_PASS=mypass
 
-    if not http and not https:
-        return None
+def get_proxy_dict():
+    host = os.getenv("PROXY_HOST")
+    port = os.getenv("PROXY_PORT")
+    user = os.getenv("PROXY_USER")
+    password = os.getenv("PROXY_PASS")
+    
+    if not host or not port:
+        return None  # без проксі
 
+    proxy_url = f"http://{user}:{password}@{host}:{port}" if user and password else f"http://{host}:{port}"
     return {
-        "http": http,
-        "https": https
+        "http": proxy_url,
+        "https": proxy_url
     }

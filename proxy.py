cb548aa45d6b4c877c16a0c900aa01394644103c
Jaguarchanski <sarakhmanjakob@gmail.com>
2025-12-21 21:29:26 +0200

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,17 +1,8 @@
-import os
+# Проксі для запитів до бірж
+HTTP_PROXY = "http://user:pass@proxy_ip:proxy_port"
+HTTPS_PROXY = "http://user:pass@proxy_ip:proxy_port"
 
-def get_proxy():
-    host = os.getenv("PROXY_HOST")
-    port = os.getenv("PROXY_PORT")
-    user = os.getenv("PROXY_USER")
-    password = os.getenv("PROXY_PASS")
-
-    if not host:
-        return None
-
-    proxy_url = f"http://{user}:{password}@{host}:{port}"
-
-    return {
-        "http": proxy_url,
-        "https": proxy_url
-    }
+PROXIES = {
+    "http": HTTP_PROXY,
+    "https": HTTPS_PROXY
+}

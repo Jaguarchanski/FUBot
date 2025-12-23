0fd381e2e7e6771f18e69bc303a8797dbfdc941f
Jaguarchanski <sarakhmanjakob@gmail.com>
2025-12-24 00:05:12 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,46 @@
 import json
 import os
+from config import EARLY_BIRD_LIMIT
 
-DB_FILE = "database.json"
+DB_FILE = "db.json"
 
-def db_load():
+def _load():
     if not os.path.exists(DB_FILE):
-        # Структура бази за замовчуванням
-        return {"early_bird_remaining": 500, "users": {}}
+        return {"users": {}, "early_used": 0}
     with open(DB_FILE, "r") as f:
         return json.load(f)
 
-def db_save(data):
+def _save(data):
     with open(DB_FILE, "w") as f:
-        json.dump(data, f, indent=4)
+        json.dump(data, f, indent=2)
+
+def get_user(user_id):
+    db = _load()
+    return db["users"].get(str(user_id))
+
+def create_user(user_id):
+    db = _load()
+    uid = str(user_id)
+
+    if uid in db["users"]:
+        return db["users"][uid]
+
+    early = db["early_used"] < EARLY_BIRD_LIMIT
+    if early:
+        db["early_used"] += 1
+
+    db["users"][uid] = {
+        "paid": False,
+        "early": early,
+        "timezone": "UTC",
+        "threshold": 1.5,
+        "exchanges": ["bybit"],
+        "notify_before": 5
+    }
+
+    _save(db)
+    return db["users"][uid]
+
+def early_left():
+    db = _load()
+    return EARLY_BIRD_LIMIT - db["early_used"]

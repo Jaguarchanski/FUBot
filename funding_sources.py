e4bef604d3bafd58dda7d47f9a56f8f6d186cd08
Jaguarchanski <sarakhmanjakob@gmail.com>
2025-12-23 23:33:16 +0200

    Update and rename funding_sources_all.py to funding_sources.py

---
+++ funding_sources.py
@@ -0,0 +1,8 @@
+# Приклад структури монет та бірж
+# В реальності тут буде API запит до бірж
+FUNDING_DATA = {
+    "Bybit": [{"pair": "BTCUSDT", "funding_rate": 0.8, "time": "12:00"}],
+    "Binance": [{"pair": "ETHUSDT", "funding_rate": 1.2, "time": "13:00"}],
+    "FTX": [{"pair": "SOLUSDT", "funding_rate": 0.5, "time": "11:30"}],
+    # Додати інші біржі
+}

df77fc871aa5e0012f5f5781f4f8dc71c206bee5
Jaguarchanski <sarakhmanjakob@gmail.com>
2025-12-27 21:25:56 +0200

    Delete proxy.py

--- proxy.py
+++
@@ -1,7 +0,0 @@
-from telegram.request import HTTPXRequest
-from config import PROXY_URL
-
-def get_request():
-    if PROXY_URL:
-        return HTTPXRequest(proxy=PROXY_URL)
-    return HTTPXRequest()

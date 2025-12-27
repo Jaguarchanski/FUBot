807ee1f4bc10590a58b74e4e0495ab4734d49df7
Jaguarchanski <sarakhmanjakob@gmail.com>
2025-12-27 21:26:17 +0200

    Delete i18n.py

--- i18n.py
+++
@@ -1,8 +0,0 @@
-MESSAGES = {
-    "welcome_free": "Привіт! Ти на стартовому тарифі. Доступна одна біржа: Bybit.",
-    "welcome_early_bird": "Привіт! Ти отримав безкоштовний тестовий місяць повного доступу. Залишилось {remaining} місць.",
-    "menu_select_timezone": "Вибери свій часовий пояс:",
-    "menu_select_exchanges": "Вибери біржі:",
-    "menu_set_funding_threshold": "Встанови мінімальний поріг фандингу:",
-    "menu_top_funding": "Топ фандинг:",
-}

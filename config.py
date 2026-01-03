7d85e2ceb9b4dee8c3bd581bdee4d4489a4c8693
Jaguarchanski <sarakhmanjakob@gmail.com>
2026-01-03 19:01:45 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # https://yourdomain.com/webhook
-PORT = int(os.getenv("PORT", 8000))
-
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # URL для FastAPI webhook
+PROXY_URL = os.getenv("PROXY_URL", None)  # якщо потрібно
 EARLY_BIRD_LIMIT = 500
-FREE_THRESHOLD = 1.5  # % funding для безкоштовного плану
-
-# Список бірж
-EXCHANGES = ["bybit", "binance", "okx", "ftx", "kucoin", "bitget", "gateio", "huobi", "mexc"]
+FREE_PLAN_THRESHOLD = 1.5  # Максимальний funding для безкоштовного плану
+FUNDING_INTERVAL = 60  # перевірка funding у секундах

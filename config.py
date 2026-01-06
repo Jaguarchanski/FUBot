bf8ebe6164b7293ac08b17e9c1a75da24bdc2e85
Jaguarchanski <sarakhmanjakob@gmail.com>
2026-01-06 20:53:55 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-FUNDING_THRESHOLD_FREE = 1.5  # % для безкоштовного плану
+class Config:
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+    PORT = int(os.getenv("PORT", 8000))
+    
+    # Ліміти за ТЗ
+    FREE_LIMIT = 1.5
+    PRO_LIMIT = 5.0
+    
+    # 9 Бірж з ТЗ
+    EXCHANGES = [
+        'binance', 'bybit', 'okx', 'bitget', 'gateio', 
+        'kucoin', 'mexc', 'huobi', 'bingx'
+    ]
+
+config = Config()
